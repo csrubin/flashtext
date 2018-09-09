5c224ca8e04f785c87849cf209306985ee57929f
Connor Rubin <csrubin@gmail.com>
2018-09-09 15:49:51 -0400

    added main function

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 15:47:41 2018
+
+@author: csrubin
+"""
+
+from datetime import datetime
+import time
+
+while True:
+    print(datetime.now())
+    time.sleep(2)
